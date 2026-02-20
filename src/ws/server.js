d32aee8467cb16b0458a7bfd662c1ecb0be96590
Durgesh <your-github-gaudd221@gmail.com>
2026-02-20 18:22:02 +0530

    implement arcjet security

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../arcjet.js"
 
 function sendJson(socket, payload) {
   if (socket.readyState !== WebSocket.OPEN) return;
@@ -13,31 +14,68 @@ function broadcast(wss, payload) {
 }
 
 export function attachWebSocketServer(server) {
-    const wss = new WebSocketServer({ noServer: true, path: '/ws', maxPayload: 1024 * 1024 });
+  const wss = new WebSocketServer({ 
+    noServer: true, 
+    path: '/ws', 
+    maxPayload: 1024 * 1024 
+  });
 
-    wss.on('connection' , (socket) => {
-      socket.isAlive = true;
-      socket.on('pong', () => { socket.isAlive = true; });
-
-      sendJson(socket, { type: 'welcome' });
-
-      socket.on('error', console.error);
-
-    })
-
-        const interval = setInterval(() => {
-        wss.clients.forEach((ws) => {
-            if (ws.isAlive === false) return ws.terminate();
-
-            ws.isAlive = false;
-            ws.ping();
-        })}, 30000);
-
-        wss.on('close', () => clearInterval(interval));
-
-    function broadcastMatchCreated(match) {
-        broadcastToAll(wss, { type: 'match_created', data: match });
+  // 🔹 REQUIRED when using noServer: true
+  server.on('upgrade', (req, socket, head) => {
+    if (req.url === '/ws') {
+      wss.handleUpgrade(req, socket, head, (ws) => {
+        wss.emit('connection', ws, req);
+      });
+    } else {
+      socket.destroy();
     }
-    return { broadcastMatchCreated };
+  });
 
+  wss.on('connection', async (socket, req) => {
+
+    if (wsArcjet) {
+      try {
+        const decision = await wsArcjet.protect(req);
+
+        if (decision.isDenied()) {
+          const code = decision.reason.isRateLimit() ? 1013 : 1008;
+          const reason = decision.reason.isRateLimit()
+            ? 'Rate Limit exceeded'
+            : 'Access denied';
+
+          socket.close(code, reason);
+          return;
+        }
+
+      } catch (e) {
+        console.error('WS connection error', e);
+        socket.close(1011, 'Server Security error');
+        return;
+      }
+    }
+
+    socket.isAlive = true;
+    socket.on('pong', () => { socket.isAlive = true; });
+
+    sendJson(socket, { type: 'welcome' });
+
+    socket.on('error', console.error);
+  });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 30000);
+
+  wss.on('close', () => clearInterval(interval));
+
+  function broadcastMatchCreated(match) {
+    broadcast(wss, { type: 'match_created', data: match });
+  }
+
+  return { broadcastMatchCreated };
 }
